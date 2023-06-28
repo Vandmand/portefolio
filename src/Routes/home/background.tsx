df3bd6754a8a29d30d83ba4984dcfb51cc61b742
Vandmand <vandmandenelias@gmail.com>
2023-06-28 18:53:42 +0200

    j

--- src/Routes/home/background.tsx
+++ src/Routes/home/background.tsx
@@ -1,8 +1,17 @@
+import {useState} from 'react';
 import { Stage, Layer, Circle, Rect } from "react-konva";
 
+
+const createRandomShapes = () => {
+  
+}
+
 const Background = () => {
-  return (
-    <Stage width={100} height={100}>
+
+  const [size, setSize] = useState({w:1000, h:1000});
+
+  return (  
+    <Stage width={size.w} height={size.h}>
       <Layer>
         <Rect width={window.innerWidth} height={window.innerHeight} fill="white"/>
         <Circle width={10} height={10} x={50} y={50} fill="FFF" />
--- src/Routes/home/front.tsx
+++ src/Routes/home/front.tsx
@@ -1,32 +1,49 @@
-import Typography from '@mui/material/Typography'
-import Background from './background'
+import { useSpring, animated } from "@react-spring/web";
+import Typography from "@mui/material/Typography";
 
-// Unused code
-const generateStrokeEffect = (inputText: string, n: number) => {
-  const strokes : JSX.Element[] = []
-  for (let i = 0; i < n; i++) {
-    strokes.push(
-      <h1 className="display stroke"></h1>
-     )
-    
-  }
-}
-
-
-
-const FrontPage = () => {
+const Title = (props: { text: string; i: number; style: any }) => {
+  const animation = useSpring({ from: { y: 0 }, to: { y: props.i**2.2 } });
 
   return (
+    <animated.div
+      style={{
+        position: "absolute",
+        zIndex: -props.i,
+        ...props.style,
+        ...animation,
+      }}
+    >
+      <Typography variant="h1" color={"primary"}>
+        {props.text}
+      </Typography>
+    </animated.div>
+  );
+};
+
+const generateTitle = (inputText: string, n: number) => {
+  const elements: JSX.Element[] = [<Title text={inputText} i={0} style={{}} key={0}></Title>];
+  for (let i = 1; i < n; i++) {
+    elements.push(<Title text={inputText} i={i} style={{
+      textStrokeColor: "black",
+      textStrokeWidth: 2,
+      color: "black" 
+    }} key={i}></Title>);
+  }
+  return elements;
+};
+
+const FrontPage = () => {
+  return (
     <div className="front-page">
-<div className="background">
-    <Background />
+      {/* <div className="background">
+        <Background />
+      </div> */}
+
+      <animated.div className={"title"}>
+        {generateTitle("Hi I'm Ellie", 10)}
+      </animated.div>
     </div>
-    
-    <Typography variant="h1" color="primary">Hi i'm ellie</Typography>
-    </div>
-    
-    
-  )
+  );
 };
 
 export default FrontPage;
